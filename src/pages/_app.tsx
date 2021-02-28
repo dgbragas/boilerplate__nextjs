04566748f9913070b888fbcbbf519fa771c1d0b7
dgbragas <dgbragas.dev@gmail.com>
2021-02-28 15:34:25 -0300

    feat: project startup with code linters and styled-components next.js config

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import { GlobalStyle } from '../styles/global';
+
+export default function App({ Component, pageProps }): JSX.Element {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components';
+
+const headings = [1, 2, 3, 4, 5, 6].map(number => `h${number}`).join(', ');
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+    box-sizing: border-box;
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 150%;
+    margin: 0;
+    outline: 0;
+    padding: 0;
+    -webkit-font-smoothing: antialiased;
+
+    ::selection {
+      background: #8257e5;
+      color: #fff;
+    }
+  }
+
+  ${headings} {
+    line-height: 133%;
+    margin: 0;
+  }
+
+  body {
+    background-color: #121214;
+    color: #fff;
+  }
+
+  body, input, button, textarea {
+    font-family: 400 1rem Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
